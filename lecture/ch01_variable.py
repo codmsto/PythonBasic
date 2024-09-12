5181acb057aa599f405a6200bd7d481fcb83551f
codmsto <166623177+codmsto@users.noreply.github.com>
2024-09-12 17:36:26 +0900

    Add files via upload

---
+++ lecture/ch01_variable.py
@@ -0,0 +1,27 @@
+# 주석(comment):실행X,메모
+print("hello")  #주석을 달아주세요
+
+# 1.출력함수(print)
+#   - ()값을 출력
+#   - 변수값 확인 용도로 많이 활용
+#   - ''(char) - 문자형
+#   - ""(string) - 문자열형
+#  Python
+#   - '' or "" - 문자열형(String)
+print('hello')
+print("hello")
+
+
+# 2.Escape Code(문자열과 함께 사용)
+#  - \n:한 줄 개행
+#  - \t:탭(기본:8칸)
+print("hello \tpython")
+print("hello \npythpn")
+
+# 3.자료형(Type)
+#  - Pyhton의 모든 자료형은 객체(Object)
+#  - 정수형(int)    : 3,-1,0
+#  - 실수형(float)  : 3.14,0.0,-2.2
+#  - 문자열(String) : "Hi",'Hi'
+#  - 논리형(Boolean): True,False
+
